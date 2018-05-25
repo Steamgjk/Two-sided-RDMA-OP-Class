453b6cd2a93a6b4066abb3180a9f7719386e967e
Your Name <you@example.com>
2018-05-25 10:18:54 +0800

    ok

--- client_op.h
+++ client_op.h
@@ -48,13 +48,13 @@ class client_op
 public:
 	client_op();
 	client_op(string lip, string rip, int rport);
-	void write_remote(struct rdma_cm_id *id, uint32_t len);
-	void post_receive(struct rdma_cm_id *id);
-	void send_next_chunk(struct rdma_cm_id *id);
-	void on_pre_conn(struct rdma_cm_id *id);
-	void on_completion(struct ibv_wc *wc);
-	void prepare_data(char* buf2send, size_t buf2send_len);
-	void run();
+	static void write_remote(struct rdma_cm_id *id, uint32_t len);
+	static void post_receive(struct rdma_cm_id *id);
+	static void send_next_chunk(struct rdma_cm_id *id);
+	static void on_pre_conn(struct rdma_cm_id *id);
+	static void on_completion(struct ibv_wc *wc);
+	static void prepare_data(char* buf2send, size_t buf2send_len);
+	static void run();
 	~client_op();
 private:
 	string local_ip;
