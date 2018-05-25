9d7703c8830261ac76fee1ffe260c40720755bae
Your Name <you@example.com>
2018-05-25 16:40:53 +0800

    makefile

--- messages.h
+++ messages.h
@@ -1,8 +1,8 @@
 #ifndef RDMA_MESSAGES_H
 #define RDMA_MESSAGES_H
 
-const char *DEFAULT_PORT = "12345";
-const size_t BUFFER_SIZE = 10 * 1024 * 1024;
+static char *DEFAULT_PORT = "12345";
+static size_t BUFFER_SIZE = 10 * 1024 * 1024;
 
 enum message_id
 {
